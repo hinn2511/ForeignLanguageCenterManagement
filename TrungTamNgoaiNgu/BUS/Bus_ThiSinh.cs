c74647049496021e97795044b3caefc533acf9df
Hin <nth251120@gmail.com>
2021-12-28 10:06:22 +0700

    add folder

---
+++ TrungTamNgoaiNgu/BUS/Bus_ThiSinh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrungTamNgoaiNgu.BUS
+{
+    internal class Bus_ThiSinh
+    {
+    }
+}
