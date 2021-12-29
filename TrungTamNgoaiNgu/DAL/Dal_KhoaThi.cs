9ec87fed87008a67306734a792ac87178082bf44
Hin <nth251120@gmail.com>
2021-12-29 22:27:59 +0700

    Add giaodienchinh

---
+++ TrungTamNgoaiNgu/DAL/Dal_KhoaThi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrungTamNgoaiNgu.DTO;
+
+namespace TrungTamNgoaiNgu.DAL
+{
+    public class Dal_KhoaThi
+    {
+        QLTTNNDataContext context = new QLTTNNDataContext();
+
+        public List<Dto_KhoaThi> DanhSachKhoaThi()
+        {
+            var dsKhoaThi = context.KhoaThis.Select(kt => new Dto_KhoaThi
+            {
+                Id = kt.Id,
+                TenKhoaThi = kt.TenKhoaThi,
+                NgayThi = kt.NgayThi,
+            }).ToList();
+            return dsKhoaThi;
+        }
+
+        public KhoaThi ChiTietKhoaThi(int id)
+        {
+            var khoaThi = context.KhoaThis.FirstOrDefault(kt => kt.Id == id);
+            return khoaThi;
+        }
+
+
+        public bool ThemKhoaThi(KhoaThi khoaThi)
+        {
+
+            context.KhoaThis.InsertOnSubmit(khoaThi);
+            try
+            {
+                context.SubmitChanges();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.InnerException);
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ TrungTamNgoaiNgu/DTO/Dto_KhoaThi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrungTamNgoaiNgu.DTO
+{
+    public class Dto_KhoaThi
+    {
+        public int Id { get; set; }
+        public string TenKhoaThi { get; set; }
+        public DateTime NgayThi { get; set; }
+    }
+}
